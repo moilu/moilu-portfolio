8de320d46a9340ae201b5cefc3add66a8d660de8
moilu <moisesluna0390@gmail.com>
2023-05-27 08:53:47 -0700

    Email sender implemented

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -5,12 +5,12 @@
                 <h1 class="fold-bold mb-4 text-4xl">Contact</h1>
                 <p>I would love to hear about your project and how I could help. Please fill in the form, and I’ll get back to you as soon as possible.</p>
             </div>
-            <form class="grid gap-4">
-                <input class="bg-transparent outline-nonen" placeholder="Name" type="text" name="name" id="">
+            <form @submit="submitForm" class="grid gap-4">
+                <input v-model="formName" class="bg-transparent outline-nonen" placeholder="Name" type="text" name="name" id="">
                 <line-break />
-                <input class="bg-transparent outline-none" placeholder="Email" type="email" name="email" id="">
+                <input v-model="formEmail" class="bg-transparent outline-none" placeholder="Email" type="email" name="email" id="">
                 <line-break />
-                <textarea class="bg-transparent resize-none outline-none" placeholder="Message" name="message" id="" cols="30" rows="5"></textarea>
+                <textarea v-model="formMessage" class="bg-transparent resize-none outline-none" placeholder="Message" name="message" id="" cols="30" rows="5"></textarea>
                 <line-break />
                 <button class="hover:animate-pulse decoration-2 decoration-custom-blue justify-self-end text-base underline underline-offset-8">SEND MESSAGE</button>
             </form>
@@ -19,11 +19,41 @@
 </template>
 <script>
 import LineBreak from './fragments/LineBreak.vue';
+import emailjs from 'emailjs-com';
 
 export default {
     name: 'ContactForm',
     components: {
         LineBreak
+    },
+    data() {
+        return {
+            formName: '',
+            formEmail: '',
+            formMessage: ''
+        }
+    },
+    methods: {
+        submitForm(e) {
+            e.preventDefault();
+            if (!this.formName || !this.formEmail || !this.formMessage) {
+                return;
+            }
+            try {
+                console.log(e.target);
+                emailjs.sendForm('service_8ati7t5', 'template_5quu2ml', e.target, 'd973Zl14QhmDTN6ed', {
+                    name: this.formName,
+                    email: this.formEmail,
+                    message: this.formMessage
+                });
+            } catch (error) {
+                console.log(error);
+            }
+            // Reset form fields
+            this.formName = '';
+            this.formEmail = '';
+            this.formMessage = '';
+        }
     }
 }
 </script>
