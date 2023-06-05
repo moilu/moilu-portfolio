89536417b7d68abf4e7c27727c14c133a3f2ec36
moilu <moisesluna0390@gmail.com>
2023-06-05 12:21:58 -0700

    New footer update

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -16,12 +16,10 @@
             </form>
         </div>
         <div class="grid justify-center mt-10 w-full md:mx-auto md:w-[90%] 2xl:w-[55%]">
-            <p></p>
-            <a class="justify-self-center hover:animate-pulse text-sm" href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
-                <span class="decoration-2 decoration-custom-blue underline underline-offset-2"> Made by Moilu</span>
-                <br>
-                Don't click this!
+            <a class="decoration-2 decoration-custom-blue underline underline-offset-2 hover:animate-pulse" href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
+                Made by Moilu
             </a>
+            <p class="justify-self-center  text-sm">Don't click this!</p>
         </div>
     </section>
 </template>
